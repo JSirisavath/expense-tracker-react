164d5fc1b1bff07a903b836fc4f5fd6d6664b8a6
Jay <fc2985up@go.minnstate.edu>
2023-04-26 18:50:40 -0500

    expenseTrackerApp

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,67 @@
+import React, { useState, useContext } from "react";
+
+import { GlobalContext } from "../context/GlobalState";
+
+export const AddTransaction = () => {
+	const [text, setText] = useState("");
+	// hooks -
+	// 1. import useState
+	// 2. [set a start state(text), then what we are manipulating ] = useState(default argument)
+	//3. Set those useState values into variables of what state corresponded of start stat in the []
+	// example : text with values of value = {text}... etc...
+	//4. onChange = {() => {e.value.target.text}} to set the text of whoever is interacting with it;
+
+	const [amount, setAmount] = useState(0);
+
+	const { addTransaction } = useContext(GlobalContext);
+
+	// when form is submitted, do this with the transaction
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		const newTransaction = {
+			//Generating an Id at random
+			id: Math.floor(Math.random() * 100000000),
+			text,
+			amount: +amount,
+		};
+
+		addTransaction(newTransaction);
+
+		setText(""); // clearing text field after submit
+		setAmount(0); // clearing amount field after submit
+	};
+	return (
+		<>
+			{" "}
+			<h3>Add new transaction</h3>
+			<form onSubmit={onSubmit} id="form">
+				<div className="form-control">
+					<label htmlFor="text">Text</label>
+					<input
+						type="text"
+						value={text}
+						onChange={
+							// Function to target what is being type in as a value {}
+							(e) => setText(e.target.value)
+						}
+						placeholder="Enter text..."
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="amount">
+						Amount <br />
+						(negative - expense, positive - income)
+					</label>
+					<input
+						type="number"
+						value={amount}
+						onChange={(e) => setAmount(e.target.value)}
+						placeholder="Enter amount..."
+					/>
+				</div>
+				<button className="btn">Add transaction</button>
+			</form>
+		</>
+	);
+};
